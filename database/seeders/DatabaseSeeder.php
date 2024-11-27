25dfdf8cc90fcd306a3831f44b05ca7b8949a5ad
thanhsang <thanhsang@gmail.com>
2024-11-27 17:00:57 +0700

    feat: Login with multi user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,15 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'user@user.com',
+            'password'=> bcrypt('123456'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password'=> bcrypt('123456'),
+            'is_admin'=>1
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Middleware\UserMiddleware;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\GuestAdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->name('admin.')->group(function(){
+
+    Route::middleware([GuestAdminMiddleware::class])->group(function(){
+        Route::get('/login', [AdminLoginController::class,'index']);
+        // Corrected here
+        Route::post('/login', [AdminLoginController::class,'login'])->name('login');
+    });
+   
+    Route::middleware(['auth', AdminMiddleware::class])->group(function(){
+        Route::get('/home', [AdminHomeController::class, 'index'])->name('home');
+    });
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 
+'index'])
+    ->name('home')
+    ->middleware(UserMiddleware::class);
